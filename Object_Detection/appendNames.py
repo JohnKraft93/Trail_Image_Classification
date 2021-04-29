7c347b1d5815ed802d537d2c0c132a85a2a3015c
Elliot Shaw <1elliot.toille1@gmail.com>
2021-04-29 09:04:29 -0600

    append py

---
+++ Object_Detection/appendNames.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+def main():
+    for filename in os.listdir(sys.argv[1]):
+        if filename.endswith(".JPG"): 
+            original = filename
+            new = sys.argv[2] + filename
+            os.rename(original, new)
+            continue
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
